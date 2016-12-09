6303cece6a717a1f7417e4865255f60c08e35a77
louisnovick <louisnovick@hotmail.com>
2016-12-08 21:19:23 -0500

    added redux store

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxagram</Link>
+        </h1>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    );
+  }
+}
+
+
---
+++ learn-redux/client/store.js
@@ -0,0 +1,18 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for default data
+const defaultState = {
+  posts,
+  comments
+};
+
+export default const store = createStore(rootReducer, defaultState);
+export default const history = syncHistoryWithStore(browserHistory, store);
